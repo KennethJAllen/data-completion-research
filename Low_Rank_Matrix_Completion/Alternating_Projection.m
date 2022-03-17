a58bb97e64d5f5ddac44bdad3abdc822eafa16e3
Kenneth <kencubed@gmail.com>
2022-03-17 13:35:21 -0400

    modivied alternating projection

--- Low_Rank_Matrix_Completion/Alternating_Projection.m
+++ Low_Rank_Matrix_Completion/Alternating_Projection.m
@@ -1,4 +1,4 @@
-function [Z,sigma] = AlternatingProjection(Omega,r, M0)
+function [X,sigma] = AlternatingProjection(Omega,r, M0)
 %This is the matrix completion algorithm based on work by Lai, Varghese
 
 %Omega = mask of unknown entries
@@ -11,13 +11,12 @@ N = 10000; %number of itterations
 sigma = zeros(1,N); %(r+1)st singular value on step k
 
 known_entries = M0(Omega);
-Z=M0;
-    %A=OR1MC(M,mOmega,r);
-    for k = 1:N
-        [U, S, V] = svd(Z);
-        Z = U(:,1:r)*S(1:r,1:r)*V(:,1:r)'; %rank r projection
-        S = diag(S);
-        sigma(k) = S(r+1);
-        Z(Omega) = known_entries; %projection onto A_Omega
-    end
+X=M0;
+for k = 1:N
+    [U, S, V] = svd(X);
+    X = U(:,1:r)*S(1:r,1:r)*V(:,1:r)'; %rank r projection
+    S = diag(S);
+    sigma(k) = S(r+1);
+    X(Omega) = known_entries; %projection onto A_Omega
+end
 end
